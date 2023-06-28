fac18752784e146c3ec958ab84777a43d5458a5d
Asgarsk08 <staging61@gmail.com>
2023-06-29 04:46:04 +0530

    Working version

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ const { google } = require("googleapis");
 const authorize = require("./authorize");
 
 const EMAIL_CHECK_INTERVAL_MIN = 0;
-const EMAIL_CHECK_INTERVAL_MAX = 1;
-const LABEL_NAME = "MyLabel";
+const EMAIL_CHECK_INTERVAL_MAX = 20;
 
 /**
  * Starts the email processing loop.
@@ -12,7 +11,8 @@ const LABEL_NAME = "MyLabel";
  */
 async function startEmailProcessing(auth) {
   setInterval(() => {
-    replyToEmails(auth)
+    checkEmails(auth)
+      .then(replyToEmailsandAddLabel)
       .catch((error) => {
         console.error("Error processing emails:", error);
       });
@@ -42,10 +42,14 @@ async function checkEmails(auth) {
   // Fetch new emails
   const response = await gmail.users.messages.list({
     userId: "me",
-    labelIds: ["INBOX","UNREAD"],
+    labelIds: ["INBOX", "UNREAD"],
   });
 
   const messages = response.data.messages || [];
+  if (messages === []) {
+    console.log("----------------------NO NEW EMAILS!-------------------");
+    return auth;
+  }
   let count = 0;
   console.log("NEW EMAILS");
   for (const message of messages) {
@@ -55,8 +59,11 @@ async function checkEmails(auth) {
       id: message.id,
     });
     count++;
-    const subject = getMessageHeaderValue(messageResponse, "Subject");
-    const body = getMessageBody(messageResponse);
+    const subject = await getMessageHeaderValue(
+      messageResponse.data.payload.headers,
+      "Subject"
+    );
+    const body = await getMessageBody(messageResponse.data);
     console.log(
       `\n+--------------------------------- EMAIL ${count} ----------------------------------------------+\n`
     );
@@ -72,126 +79,136 @@ async function checkEmails(auth) {
   return auth;
 }
 
+async function replyToEmailsandAddLabel(auth) {
+  try {
+    const gmail = google.gmail({ version: "v1", auth });
+
+    // Fetch emails
+    const response = await gmail.users.messages.list({
+      userId: "me",
+      labelIds: ["INBOX"],
+    });
+    const messages = response.data.messages || [];
+
+    for (const message of messages) {
+      const threadResponse = await gmail.users.threads.get({
+        userId: "me",
+        id: message.threadId,
+      });
+      const messageResponse = await gmail.users.messages.get({
+        userId: "me",
+        id: message.id,
+      });
+      const thread = threadResponse.data;
+      const messagesInThread = thread.messages || [];
+      console.log(messagesInThread.length);
+      // If no prior replies, send a reply
+      if (messagesInThread.length === 1) {
+        const { subject, to, messageId } = getMessageHeaders(messageResponse);
+        const replyMessage = createReplyMessage(
+          subject,
+          to,
+          messageId,
+          message.threadId
+        );
+        const reply_sent_response = await sendMessage(auth, replyMessage);
+        console.log("Reply sent to message ID:", message.id);
+
+        // Add a label to the sent reply
+        const labelName = "PREMIUM"; // Replace with your desired label name
+        const labelId = await getOrCreateLabel(auth, labelName);
+
+        const response_label_add = await gmail.users.messages.modify({
+          auth: auth,
+          userId: "me",
+          id: reply_sent_response.data.id,
+          resource: {
+            addLabelIds: [labelId],
+          },
+        });
+      } else {
+        console.log(
+          "Reply cannot sent to this message:",
+          message.id,
+          " as the chat has more than or equal to 2 threads"
+        );
+      }
+    }
+  } catch (error) {
+    console.error("Error replying to emails:", error);
+  }
+}
+
 /**
- * Sends a reply to emails that have no prior replies.
+ * Retrieves the relevant headers from a message.
+ *
+ * @param {object} message The message object.
+ * @returns {object} The relevant headers.
+ */
+function getMessageHeaders(message) {
+  const headers = message.data.payload.headers || [];
+  let subject = "";
+  let to = "";
+  let messageId = "";
+
+  for (const header of headers) {
+    const name = header.name.toLowerCase();
+    if (name === "subject") {
+      subject = header.value;
+    } else if (name === "from") {
+      to = header.value;
+    } else if (name === "message-id" || name === "message-id") {
+      messageId = header.value;
+    }
+  }
+
+  return { subject, to, messageId };
+}
+
+/**
+ * Sends a reply message.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- * @returns {Promise<google.auth.OAuth2>} An authorized OAuth2 client.
+ * @param {object} replyMessage The reply message object.
+ * @returns {Promise<void>} A promise that resolves when the reply is sent.
  */
-async function replyToEmails(auth) {
+async function sendMessage(auth, replyMessage) {
   const gmail = google.gmail({ version: "v1", auth });
 
-  // Fetch emails
-  const response = await gmail.users.messages.list({
-    userId: "me",
-    labelIds: ["INBOX","UNREAD"],
-  });
-
-  const messages = response.data.messages || [];
-  
-  for (const message of messages) {
-    console.log("Message,content",message);   
-    // Check if the email has any prior replies
-    const threadResponse = await gmail.users.threads.get({
-      userId: "me",
-      id: message.threadId,
-    });
-
-    const thread = threadResponse.data;
-    const messagesInThread = thread.messages || [];
-
-    // If no prior replies, send a reply
-    if (messagesInThread.length === 1) {
-      const replyMessage = createReplyMessage(message);
-      await gmail.users.messages.send({
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.send(
+      {
         userId: "me",
         requestBody: replyMessage,
-      });
-      console.log("Reply sent:", message.id);
-    }
-    break;
-  }
-
-  return auth;
-}
-
-/**
- * Adds a label to the emails and moves them to the label.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- * @returns {Promise<google.auth.OAuth2>} An authorized OAuth2 client.
- */
-async function addLabelToEmails(auth) {
-  const gmail = google.gmail({ version: "v1", auth });
-
-  // Get or create the label
-  const labelResponse = await gmail.users.labels.list({ userId: "me" });
-  const labels = labelResponse.data.labels || [];
-  let labelId = null;
-
-  for (const label of labels) {
-    if (label.name === LABEL_NAME) {
-      labelId = label.id;
-      break;
-    }
-  }
-
-  if (!labelId) {
-    const createLabelResponse = await gmail.users.labels.create({
-      userId: "me",
-      requestBody: {
-        label: {
-          name: LABEL_NAME,
-          labelListVisibility: "labelShow",
-          messageListVisibility: "show",
-        },
       },
-    });
-
-    labelId = createLabelResponse.data.id;
-    console.log("Label created:", LABEL_NAME);
-  }
-
-  // Apply the label to the emails
-  const response = await gmail.users.messages.list({
-    userId: "me",
-    labelIds: ["INBOX"],
+      (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      }
+    );
   });
-
-  const messages = response.data.messages || [];
-
-  for (const message of messages) {
-    await gmail.users.messages.modify({
-      userId: "me",
-      id: message.id,
-      requestBody: {
-        addLabelIds: [labelId],
-        removeLabelIds: ["INBOX"],
-      },
-    });
-    console.log("Label added to email:", message.id);
-  }
-
-  return auth;
 }
 
 /**
- * Creates a reply message.
+ * Creates a reply message object.
  *
- * @param {object} message The original message object.
+ * @param {string} subject The subject of the original message.
+ * @param {string} to The recipient of the original message.
+ * @param {string} inReplyTo The Message-ID of the original message.
+ * @param {string} threadId thread-ID to which reply email should be hooked
  * @returns {object} The reply message object.
  */
-function createReplyMessage(message) {
-  const replySubject = "Re: " + getMessageHeaderValue(message, "Subject");
-  const replyBody = "Thank you for your email. This is an automated reply from Mohammed Faisal";
+function createReplyMessage(subject, to, inReplyTo, threadId) {
+  const replySubject = "Re: " + subject;
+  const replyBody =
+    "Thank you for your email. This is an automated reply from Mohammed Faisal";
 
   return {
-    threadId: message.threadId,
-    requestBody: {
-      message: {
-        raw: createRawMessage(getMessageHeaderValue(message, "From"), replySubject, replyBody),
-      },
-    },
+    raw: makeBody(to, replySubject, replyBody, inReplyTo),
+    threadId: threadId,
   };
 }
 
@@ -201,92 +218,76 @@ function createReplyMessage(message) {
  * @param {string} to The recipient of the message.
  * @param {string} subject The subject of the message.
  * @param {string} body The body content of the message.
+ * @param {string} inReplyTo The Message-ID of the original message.
  * @returns {string} The raw message string.
  */
-function createRawMessage(to, subject, body) {
-  const messageParts = [
-    `To: ${to}`,
-    "Content-Type: text/plain; charset=\"UTF-8\"",
-    "MIME-Version: 1.0",
-    `Subject: ${subject}`,
-    "",
+function makeBody(to, subject, body, inReplyTo) {
+  const str = [
+    'Content-Type: text/plain; charset="UTF-8"\n',
+    "MIME-Version: 1.0\n",
+    "Content-Transfer-Encoding: 7bit\n",
+    "References: " + inReplyTo + "\n",
+    "In-Reply-To: " + inReplyTo + "\n",
+    "To: " + to + "\n",
+    "Subject: " + subject + "\n\n",
     body,
-  ];
+  ].join("");
 
-  return Buffer.from(messageParts.join("\n")).toString("base64");
+  const encodedMail = Buffer.from(str)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+  return encodedMail;
 }
 
-/**
- * Retrieves a specific header value from a message.
- *
- * @param {object} message The message object.
- * @param {string} headerName The name of the header.
- * @param {object} gmail An instance of the Gmail API client.
- * @returns {Promise<string|null>} A promise that resolves with the value of the header, or null if not found.
- */
-async function getMessageHeaderValue(message, headerName, gmail) {
-    if (!message || !message.id) {
-      return null;
-    }
-  
-    try {
-      const response = await gmail.users.messages.get({
-        userId: 'me',
-        id: message.id,
-      });
-  
-      const headers = response.data.payload.headers || [];
-  
-      for (const header of headers) {
-        if (header.name.toLowerCase() === headerName.toLowerCase()) {
-          return header.value;
-        }
-      }
-  
-      return null;
-    } catch (error) {
-      console.error('Error retrieving message details:', error);
-      return null;
-    }
+function getMessageHeaderValue(headers, name) {
+  const header = headers.find(
+    (header) => header.name.toLowerCase() === name.toLowerCase()
+  );
+  return header ? header.value : null;
+}
+
+function getMessageBody(msgData) {
+  if (msgData && msgData.snippet) {
+    const body = msgData.snippet;
+    return body;
   }
-  
-  
-/**
- * Retrieves the body content of a message.
- *
- * @param {object} message The message object.
- * @param {object} gmail An instance of the Gmail API client.
- * @returns {Promise<string>} A promise that resolves with the body content of the message.
- */
-async function getMessageBody(message, gmail) {
-    if (!message || !message.id) {
-      return "";
-    }
-  
-    try {
-      const response = await gmail.users.messages.get({
-        userId: "me",
-        id: message.id,
-        format: "full",
-      });
-  
-      const parts = response.data.payload.parts || [];
-      const bodyPart = parts.find((part) => part.mimeType === "text/plain");
-  
-      if (bodyPart) {
-        const body = bodyPart.body;
-        if (body.size && body.data) {
-          return Buffer.from(body.data, "base64").toString("utf-8");
-        }
-      }
-  
-      return "";
-    } catch (error) {
-      console.error("Error retrieving message body:", error);
-      return "";
-    }
+  return null;
+}
+
+// Function to create or get the label ID based on label name
+async function getOrCreateLabel(auth, labelName) {
+  const gmail = google.gmail({ version: "v1", auth });
+
+  // Get the list of all labels
+  const labelsResponse = await gmail.users.labels.list({
+    auth: auth,
+    userId: "me",
+  });
+
+  const labels = labelsResponse.data.labels;
+  let labelId = "";
+
+  // Check if the label already exists
+  const existingLabel = labels.find((label) => label.name === labelName);
+  if (existingLabel) {
+    labelId = existingLabel.id;
+  } else {
+    // Create a new label
+    const createLabelResponse = await gmail.users.labels.create({
+      auth: auth,
+      userId: "me",
+      resource: {
+        name: labelName,
+        labelListVisibility: "labelShow",
+        messageListVisibility: "show",
+      },
+    });
+    labelId = createLabelResponse.data.id;
   }
-  
+
+  return labelId;
+}
 
 // Main function
 authorize()
